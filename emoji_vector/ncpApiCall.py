0aaee7a7283102f17a24771fd89e186e28b4eef3
김지훈 <80799163+xxxjjhhh@users.noreply.github.com>
2024-04-11 09:56:00 +0900

    Edit ncpApiCall.py

--- emoji_vector/ncpApiCall.py
+++ emoji_vector/ncpApiCall.py
@@ -5,10 +5,10 @@ import http.client
 class NcpApiCall():
     def __init__(self) -> None:
         self.__host = 'clovastudio.apigw.ntruss.com'
-        self.__path = ''
-        self.__api_key = ''
-        self.__api_key_primary_val = ''
-        self.__request_id = ''
+        self.__path = '/testapp/v1/api-tools/embedding/clir-emb-dolphin/a94490cc05eb4467859045836eb77d04'
+        self.__api_key = 'NTA0MjU2MWZlZTcxNDJiY1hwQAv1f9N2hXCKeNLejej4xsafLclhA+rTcTphvp8q'
+        self.__api_key_primary_val = 'k5TdtxTKiZi4O7gi6uidLBXnqzU4eG0vmBfF9Ys0'
+        self.__request_id = 'f0fda080-70d1-4d3d-8edd-b6f9fd0cb1e0'
 
     def __send_request(self, completion_request) -> any:
         headers = {
