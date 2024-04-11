28796812706b71c13ec3124df0a5869d806e8d1e
김지훈 <80799163+xxxjjhhh@users.noreply.github.com>
2024-04-11 09:26:47 +0900

    Add jsonExecutor.py

---
+++ emoji_vector/jsonExecutor.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+import json
+
+class JonsExecutor():
+    def __init__(self) -> None:
+        self.__read_path = '../asset/emoji_korean.json'
+        self.__write_path = '../asset/emoji_vector.json'
+
+    def read_file(self) -> list:
+        with open(self.__read_path) as file:
+            json_data = json.load(file)
+
+        return json_data
+    
+    def write_file(self, list_data) -> None:
+        with open(self.__write_path, 'w', encoding='utf-8') as file:
+            file.write(json.dumps(list_data))
