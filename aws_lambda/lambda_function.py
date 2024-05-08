86b2d60d453c77475d63cc832d3db0368e0350bc
김지훈 <80799163+xxxjjhhh@users.noreply.github.com>
2024-05-09 02:54:15 +0900

    Create lambda_function.py

---
+++ aws_lambda/lambda_function.py
@@ -0,0 +1,44 @@
+import json
+import math
+
+from ncpApiCall import NcpApiCall
+
+ncpApiCall = NcpApiCall()
+
+def distance(vector1: list, vector2: list) -> float:
+    
+    sum_of_squares = sum((v2 - v1) ** 2 for v1, v2 in zip(vector1, vector2))
+    
+    return math.sqrt(sum_of_squares)
+
+def call_api(hexcode: str) -> list:
+    
+    request_data = json.loads(f'{{"text": "{hexcode}"}}', strict=False)
+    # call api, res : list
+    return ncpApiCall.execute(request_data)
+    
+
+def lambda_handler(event, context):
+    
+    request_body = json.loads(event['body'])
+    hexcode = request_body.get('hexcode', None)
+    
+    response_data = call_api(hexcode)
+    
+    with open("./emoji_vector.json") as file:
+        candidate_data = json.load(file)
+
+    dist_list = []
+    for row in candidate_data:
+
+        dist_data = {"hexcode": row['hexcode'], "distance": distance(row['vector'], response_data)}
+        dist_list.append(dist_data)
+
+    min_distance_row = min(dist_list, key=lambda x: float(x["distance"]))
+    min_hexcode = min_distance_row['hexcode']
+    
+    # TODO implement
+    return {
+        'statusCode': 200,
+        'body': json.dumps(min_hexcode)
+    }
